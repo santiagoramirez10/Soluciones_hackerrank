82e76549a6dd40511e4f98b581dfa8c6327dfaa4
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2023-12-03 19:29:05 -0500

    All basics

--- SQL/Basic.sql
+++ SQL/Basic.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 --Revising the Select Query I
 SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
 --Revising the Select Query II
@@ -17,6 +16,11 @@ SELECT CITY, STATE FROM STATION;
 SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
 --Weather Observation Station 4
 SELECT COUNT(*) - COUNT(DISTINCT CITY) AS Difference FROM STATION;
+--Weather Observation Station 5
+SELECT city, LENGTH(city) as length FROM STATION ORDER BY length ASC, city LIMIT 1;
+SELECT city, LENGTH(city) as length FROM STATION ORDER BY length DESC, city LIMIT 1;
+--Weather Observation Station 6
+SELECT DISTINCT city FROM STATION WHERE LOWER(SUBSTRING(city, 1, 1)) IN ('a', 'e', 'i', 'o', 'u');
 --Weather Observation Station 7
 SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY, 1) IN ('a', 'e', 'i', 'o', 'u');
 --Weather Observation Station 8
@@ -32,39 +36,6 @@ SELECT DISTINCT CITY FROM STATIO WHERE (LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', '
 --Higher Than 75 Marks
 SELECT Name FROM STUDENTS WHERE Marks > 75 ORDER BY RIGHT(Name, 3), ID;
 --Employee Names
-=======
---Revising the Select Query I
-SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
---Revising the Select Query II
-SELECT NAME FROM CITY WHERE COUNTRYCODE='USA' AND POPULATION>120000;
---Select All
-SELECT * FROM CITY;
---Select By ID
-SELECT * FROM CITY WHERE ID=1661;
---Japanese Cities' Attributes
-SELECT * FROM CITY WHERE COUNTRYCODE='JPN';
---Japanese Cities' Names
-SELECT NAME FROM CITY WHERE COUNTRYCODE='JPN';
---Weather Observation Station 1
-SELECT CITY, STATE FROM STATION;
---Weather Observation Station 3
-SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
---Weather Observation Station 4
-SELECT COUNT(*) - COUNT(DISTINCT CITY) AS Difference FROM STATION;
---Weather Observation Station 7
-SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY, 1) IN ('a', 'e', 'i', 'o', 'u');
---Weather Observation Station 8
-SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u') AND LOWER(RIGHT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u');
---Weather Observation Station 9
-SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
---Weather Observation Station 10
-SELECT DISTINCT CITY FROM STATION WHERE LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
---Weather Observation Station 11
-SELECT DISTINCT CITY FROM STATION WHERE (LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')) OR (LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u'));
---Weather Observation Station 12
-SELECT DISTINCT CITY FROM STATIO WHERE (LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')) AND (LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u'));
---Higher Than 75 Marks
-SELECT Name FROM STUDENTS WHERE Marks > 75 ORDER BY RIGHT(Name, 3), ID;
---Employee Names
->>>>>>> d41bb79ce517675e44df906e7dfc4d6801ae25c3
 SELECT name FROM Employee ORDER BY name;
+--Employee Salaries
+SELECT name FROM Employee WHERE salary > 2000 AND months <10 ORDER BY employee_id ASC;
